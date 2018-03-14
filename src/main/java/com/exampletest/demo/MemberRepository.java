ee9d5c129775624158ae105551a9c1fb9037fb1c
LeeJaechan <didrod446@gmail.com>
2018-03-14 20:01:13 +0900

    Initial commit

---
+++ src/main/java/com/exampletest/demo/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.exampletest.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
